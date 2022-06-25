5e21c961fd34b6180bbf75bb687bf976653dcd66
ChillFish8 <hburt2003@gmail.com>
2022-06-25 23:00:14 +0100

    Close menu on click

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {fly} from 'svelte/transition';
+    import {page} from'$app/stores';
     import List, {Item, Separator, Text} from '@smui/list';
     import SignIn from "./buttons/SignIn.svelte";
     import {PREMIUM_URL} from "$lib/api/config";
@@ -7,9 +7,12 @@
     import {Hamburger} from "svelte-hamburgers";
 
     let menuOpen = false;
+    page.subscribe(() => {
+        menuOpen = false;
+    });
 </script>
 
-<div class="absolute lg:relative top-0 left-0 bg-brand-darker shadow-hard-left w-full lg:w-1/4 max-w-none lg:max-w-300 {menuOpen && 'min-h-screen'} lg:h-full lg:min-h-screen px-0 lg:px-4 pb-2 pt-4 lg:py-8 z-50">
+<div class="absolute lg:relative top-0 left-0 bg-brand-darker shadow-md lg:shadow-hard-left w-full lg:w-1/4 max-w-none lg:max-w-300 {menuOpen && 'min-h-screen'} lg:h-full lg:min-h-screen px-0 lg:px-4 pb-2 pt-4 lg:py-8 z-50">
     <div class="flex items-center justify-between lg:justify-center w-full border-b border-brand-orange pb-4 px-4 lg:px-0">
         <div class="flex items-center space-x-4">
             <img class="w-10 h-10 rounded-full" src="/icons/logo.webp" alt=""/>
